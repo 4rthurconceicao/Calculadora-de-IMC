bdd46147e0aa9acd6dd7d5a737093c6541cdef4b
4rthurconceicao <arthurconceicaosouza1@gmail.com>
2025-04-18 16:57:16 -0300

    add

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-function Calcular (){
+function Calcular() {
 let nome = document.getElementById('nome')
 let kg = Number(document.getElementById('peso'))
 let m = Number(document.getElementById('altura'))
-let IMC = kg (m*m)
+let IMC = kg/(m*m)
+
 document.getElementById('res').innerHTML =  `Olá ${nome.value} o seu imc e`
-if (IMC >= 18.55 && IMC <= 24.9) { 
-    document.getElementById('res')+= innerHTML = `${IMC} voce está com o peso adequado`
-}
+if (IMC < 18.5) { 
+    res.innerHTML += `${IMC.toFixed(2)} voce está com o peso adequado`
+}else if (IMC >= 18.5 && IMC <= 24.9){
+
+}else if (IMC >= 25.0 && IMC <= 29.9){
+
+}else if (IMC >= 30.00 && IMC <= 34.9 ){
+
+}else if (IMC >= 35.0 && IMC <= 39.9){
+
+}else if (IMC >= 40)
 
 }
